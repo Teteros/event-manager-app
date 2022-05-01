4d81a71b575fa275c7d647bfa9c9f9d3c5c288e3
Pawel (20070573) <Teteros@users.noreply.github.com>
2022-04-30 20:00:40 -0700

    Add Menu Skeleton
    Implements ScannerInput utility, Linting & Logger support
    
    as well as the following functions:
    addEvent()
    listEvents()
    updateEvent()
    deleteEvent()
    bookVenue()
    searchEvent()
    save()
    load()
    exitApp()

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.21"
+    // Linting Support
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
     application
 }
 
@@ -14,6 +16,10 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+
+    // Logging Support
+    implementation("io.github.microutils:kotlin-logging:2.1.21")
+    implementation("org.slf4j:slf4j-simple:1.7.36")
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,86 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import mu.KotlinLogging
+import utils.ScannerInput.readNextInt
+import kotlin.system.exitProcess
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+private val logger = KotlinLogging.logger {}
+
+fun main() {
+    runMenu()
+}
+
+fun runMenu() {
+    do {
+        when (mainMenu()) {
+            1 -> addEvent()
+            2 -> listEvents()
+            3 -> updateEvent()
+            4 -> deleteEvent()
+            5 -> bookVenue()
+            6 -> searchEvent()
+            7 -> save()
+            8 -> load()
+            0 -> exitApp()
+            else -> logger.error {"Please choose an option from 0-8"}
+        }
+    } while (true)
+}
+
+fun mainMenu(): Int {
+    return readNextInt(
+        """
+         > -------------------------
+         > |   EVENT MANAGER APP   |
+         > -------------------------
+         > |   1) Add Event        |
+         > |   2) List all Events  |
+         > |   3) Update Event     |
+         > |   4) Delete Event     |
+         > -------------------------
+         > |   5) Book a Venue     |
+         > |   6) Search Events    |
+         > -------------------------
+         > |   7) Save             |
+         > |   8) Load             |
+         > -------------------------
+         > |   0) Exit             |
+         > -------------------------
+         > ==>> """.trimMargin(">")
+    )
+}
+
+fun addEvent() {
+
+}
+
+fun listEvents() {
+
+}
+
+fun updateEvent() {
+
+}
+
+fun deleteEvent() {
+
+}
+
+fun bookVenue() {
+
+}
+
+fun searchEvent() {
+
+}
+
+fun save() {
+
+}
+
+fun load() {
+
+}
+
+fun exitApp() {
+    logger.info {"exitApp() function invoked"}
+    exitProcess(0)
 }
